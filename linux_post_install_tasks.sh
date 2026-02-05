385d16334f58465fc92e52dfee37b6c8753697d9
Parag Kalra <paragkalra@gmail.com>
2026-02-05 15:41:37 -0800

    Created a post install task for Linux.

---
+++ linux_post_install_tasks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt update
+
+# Google chrome
+
+# zoom
+
+# 1Password
+sudo apt install 1password -y
+
+# minikube
+
+# gcloud cli
+
+# aws cli
+
+# terminator
+sudo apt install terminator -y
+
+# curl
+sudo apt install curl -y
+
+# Go
+sudo apt install golang-go -y
+
+# Gedit
+sudo apt install gedit -y
